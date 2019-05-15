ddf91f31041d0b71ee211f793e79f0933c0c599f
Torben Knerr <mail@tknerr.de>
2019-05-15 20:20:07 +0200

    add platform specifics

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,7 +8,14 @@ service 'apache2' do
 end
 
 file '/var/www/html/index.html' do
-  content "hello from #{node['myapp']['greeter']}!"
+  content <<~HTML
+    <html>
+        <body>
+            Hello from #{node['myapp']['greeter']}!<br/>
+            (sent via #{node['platform']} #{node['platform_version']})
+        </body>
+    </html>
+  HTML
 end
 
 log 'open your browser at http://localhost:8080 to see it running'
