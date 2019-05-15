55cdf560708424722842632ecbc770245d88318e
Torben Knerr <mail@tknerr.de>
2019-05-15 20:14:29 +0200

    fixed cookstyle issues using 'cookstyle -- auto-correct'

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,14 +1,14 @@
 
-package "apache2" do
-	action :install
+package 'apache2' do
+  action :install
 end
 
-service "apache2" do
-	action [:enable, :start]
+service 'apache2' do
+  action [:enable, :start]
 end
 
-file "/var/www/html/index.html" do
-	content "hello world!"
+file '/var/www/html/index.html' do
+  content 'hello world!'
 end
 
-log "open your browser at http://localhost:8080 to see it running"
+log 'open your browser at http://localhost:8080 to see it running'
