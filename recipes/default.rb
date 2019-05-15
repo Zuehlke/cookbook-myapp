e34312fa2634ea58d75aa485a4ec47a3cab06ea3
Torben Knerr <mail@tknerr.de>
2019-05-15 20:13:19 +0200

    add initial recipe for myapp

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,1 +1,14 @@
 
+package "apache2" do
+	action :install
+end
+
+service "apache2" do
+	action [:enable, :start]
+end
+
+file "/var/www/html/index.html" do
+	content "hello world!"
+end
+
+log "open your browser at http://localhost:8080 to see it running"
