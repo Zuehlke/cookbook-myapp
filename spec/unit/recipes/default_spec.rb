d8320e776dac904fe54b5fabdc9ca5de2e35850e
Torben Knerr <mail@tknerr.de>
2019-05-16 18:12:43 +0200

    implement actual chefspec tests

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -27,29 +27,18 @@
 require 'spec_helper'
 
 describe 'myapp::default' do
-  context 'When all attributes are default, on Ubuntu 16.04' do
+  context 'When all attributes are default, on Ubuntu 18.04' do
     let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '18.04')
       runner.converge(described_recipe)
     end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
-    end
-  end
-
-  context 'When all attributes are default, on CentOS 7.4.1708' do
-    let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708')
-      runner.converge(described_recipe)
+    it 'would install apache2 package' do
+      expect(chef_run).to install_package('apache2')
     end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'renders the correct platform version in index.html' do
+      expect(chef_run).to render_file('/var/www/html/index.html').with_content "ubuntu 18.04"
     end
   end
 end
