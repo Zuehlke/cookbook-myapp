0d6f285c436ebd707a11f2ab66868d77bc93a072
Torben Knerr <mail@tknerr.de>
2019-05-16 18:36:23 +0200

    autocorrect quotes via cookstyle

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -30,21 +30,21 @@ describe 'myapp::default' do
   context 'on Ubuntu' do
     platform 'ubuntu'
     it { is_expected.to install_package('apache2') }
-    it { is_expected.to render_file('/var/www/html/index.html').with_content "Hello from john doe!" }
+    it { is_expected.to render_file('/var/www/html/index.html').with_content 'Hello from john doe!' }
 
     context 'with custom greeter configured' do
       default_attributes['myapp']['greeter'] = 'peter parker'
-      it { is_expected.to render_file('/var/www/html/index.html').with_content "Hello from peter parker" }
+      it { is_expected.to render_file('/var/www/html/index.html').with_content 'Hello from peter parker' }
     end
 
     context 'on 18.04' do
       platform 'ubuntu', '18.04'
-      it { is_expected.to render_file('/var/www/html/index.html').with_content "ubuntu 18.04" }
+      it { is_expected.to render_file('/var/www/html/index.html').with_content 'ubuntu 18.04' }
     end
 
     context 'on 16.04' do
       platform 'ubuntu', '16.04'
-      it { is_expected.to render_file('/var/www/html/index.html').with_content "ubuntu 16.04" }
+      it { is_expected.to render_file('/var/www/html/index.html').with_content 'ubuntu 16.04' }
     end
   end
 end
