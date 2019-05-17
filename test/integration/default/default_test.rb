193244045810835345f86a01604f247706708e40
Torben Knerr <mail@tknerr.de>
2019-05-17 10:59:42 +0200

    add kitchenci / inspec tests for the desired state

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -3,14 +3,20 @@
 # The InSpec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root') do
-    it { should exist }
-  end
+describe package('apache2') do
+  it { should be_installed }
 end
 
-# This is an example test, replace it with your own test.
+describe service('apache2') do
+  it { should be_enabled   }
+  it { should be_running   }
+end
+
 describe port(80) do
-  it { should_not be_listening }
+  it { should be_listening }
+end
+
+describe file('/var/www/html/index.html') do
+  it { should be_file }
+  it { should contain 'Hello from john doe!' }
 end
